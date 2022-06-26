606cae4294be3225ba61d05a5c7c55f7fe51e0b4
CryptoSupportIssues <76901075+Biadedigital@users.noreply.github.com>
2022-06-26 15:20:22 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function changeImage(event){
+  document.querySelector(".pro-img").src=event.children[0].src
+  
+  for(let i=0; i<thumbs.length;i++){
+    thumbs[i].classList.remove("active");
+  }
+  event.classList.add("active");
+}
